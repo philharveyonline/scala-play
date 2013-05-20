f74b0aa38366b2487ae4940b10d31f88b88b096c
Phil Harvey <phil@philharveyonline.com>
2013-05-20 07:13:12 +0100

    added simple scala files

---
+++ src/main/scala/com/philharveyonline/scalaplay/MyClass.scala
@@ -0,0 +1,8 @@
+package com.philharveyonline
+
+class MyClass {
+
+  def doStuff : String = {
+    return "hello world"
+  }
+}
---
+++ src/main/scala/com/philharveyonline/scalaplay/ScalaPlayApp.scala
@@ -0,0 +1,8 @@
+package com.philharveyonline
+
+object ScalaPlayApp extends App {
+
+  val myClass = new MyClass()
+  val result = myClass.doStuff
+  println(result)
+}
